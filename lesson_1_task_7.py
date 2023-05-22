22c6940de9c49edcc7f1e3cdce715223e6f25d91
roza-karapetyan <rosakarapetian@gmail.com>
2023-05-23 02:58:22 +0300

    lesson_1_task_7

---
+++ lesson_1_task_7.py
@@ -0,0 +1,47 @@
+def first_num():
+    print(1, end='')
+
+
+def second_num():
+    print(2, end='')
+
+
+def third_num():
+  print(3, end='')
+
+
+def fourth_num():
+    print(4, end='')
+
+
+def fifth_num():
+    print(5, end='')
+
+
+def sixth_num():
+    print(6, end='')
+
+def seventh_num():
+    print(7, end='')
+
+def eighth_num():
+    print(8, end='')
+
+def ninth_num():
+    print(9, end='')
+
+def tenth_num():
+    print(0, end='')
+
+
+eighth_num()
+eighth_num()
+tenth_num()
+tenth_num()
+fifth_num()
+fifth_num()
+fifth_num()
+third_num()
+fifth_num()
+third_num()
+fifth_num()
